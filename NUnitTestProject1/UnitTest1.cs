f4803e0819c3daf7433e3f65416f65666d49b396
LapusteAlexandru <lapuste.alex92@gmail.com>
2019-11-13 22:07:44 +0200

    add chrome

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,20 +1,28 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
 
 namespace NUnitTestProject1
 {
     public class Tests
     {
+        IWebDriver driver;
         [SetUp]
         public void Setup()
         {
+            var cap = new ChromeOptions();
+            cap.AddArgument("--start-maximized");
+            IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), cap);
         }
 
         [Test]
         public void Test1()
         {
-            Console.WriteLine("Test");
-            Assert.Pass();
+            IWebElement search=driver.FindElement(By.Name("q"));
+            search.SendKeys("Selenium");
+            search.Submit();
         }
     }
 }
